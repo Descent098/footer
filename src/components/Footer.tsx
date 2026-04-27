2eee9221b28eef391767f961b8c1bbbff2d32227
Kieran Wood <kieran@canadiancoding.ca>
2026-04-27 11:10:59 -0600

    Added license notice to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,6 +20,7 @@ export default ((opts?: FooterOptions) => {
           {i18n(cfg?.locale ?? "en-US").components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz</a> &copy; {year}
         </p>
+        <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://kieranwood.ca/compsci/">The Compsci KB</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://kieranwood.ca">Kieran Wood</a> is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:middle;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""/><img style="height:22px!important;margin-left:3px;vertical-align:middle;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""/><img style="height:22px!important;margin-left:3px;vertical-align:middle;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""/></a></p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
